2d148aed9f6171bc2037533a7fb3bd79808c9c1f
Fredy Velásquez <macacoloco696@gmail.com>
2024-06-06 22:31:43 -0600

    ds

--- assets/js/arreglos.js
+++ assets/js/arreglos.js
@@ -1,1 +1,14 @@
-console.log('hola')
+//console.log('hola')
+
+//forma basica de crear arrays
+let videojuegos = ["Mario 3", "Megaman", "Chrono Trigger"];
+
+console.log({videojuegos})
+console.log(videojuegos[0])
+
+let arregloCosas = [
+    true,
+    123,
+    'Fredy',
+    1 + 2
+]
