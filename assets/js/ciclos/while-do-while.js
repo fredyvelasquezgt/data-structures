cccbdf0ef64e717cb4fd9104db5b2c250bf0ab21
Fredy Velásquez <macacoloco696@gmail.com>
2024-06-17 00:22:52 -0600

    ciclos

--- assets/js/ciclos/while-do-while.js
+++ assets/js/ciclos/while-do-while.js
@@ -3,7 +3,28 @@ const carros = ['Ford', 'Mazda', 'Honda', 'Toyota'];
 
 let i = 0;
 
-while( i < carros.length ) {
+// while( i < carros.length ) {
+//     console.log(carros[i])
+//     i++;
+// }
+
+while(carros[i]) {
+    if(i === 1){
+        i++;
+        continue;
+    }
+
     console.log(carros[i])
     i++;
 }
+
+
+let j = 0;
+
+do{
+
+    console.log(carros[j])
+    j++;
+
+
+} while(carros[j]);
